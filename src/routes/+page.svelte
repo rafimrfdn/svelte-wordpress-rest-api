63778f0f44ef80f912269409a62fa049d5ec0ee1
rafimrfdn <uphy.mks@gmail.com>
2023-01-22 22:54:56 +0800

    menambahkan link title, thumbnail dan except

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,9 +12,19 @@
 <h1>Fetch API Wordpress using SVELTE</h1>
 
 <p>Here is the list articles: </p>
-<ul>
-  {#each posts as post}
-    <li>{post.title.rendered}</li>
-  {/each}
-</ul>
 
+ <main class="container">
+  {#each posts as post}
+    <article>
+      <header>
+      <h3><a href="{post.link}">{post.title.rendered}</a></h3>
+
+      <img src="{post.jetpack_featured_media_url}" alt="{post.title.rendered}"/>
+              </header>
+
+      {post.excerpt.rendered}
+
+    </article>
+  {/each}
+
+ </main>
