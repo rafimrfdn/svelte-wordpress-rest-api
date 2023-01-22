fba7eb09e30c72c6830e1ef7916d159d06fe7cc4
rafimrfdn <uphy.mks@gmail.com>
2023-01-22 18:12:18 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from 'svelte';
+  
+  let posts = [];
+
+  onMount(async () => {
+    const res = await fetch('https://example.com/wp-json/wp/v2/posts');
+    posts = await res.json();
+  });
+</script>
+
+<h1>Fetch API Wordpress using SVELTE</h1>
+
+<p>Here is the list articles: </p>
+<ul>
+  {#each posts as post}
+    <li>{post.title.rendered}</li>
+  {/each}
+</ul>
+
